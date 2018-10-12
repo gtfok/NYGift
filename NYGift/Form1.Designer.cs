36056c94db63efc8dfdc59e6dadfde360fbba3a4
BuildTools <unconfigured@null.spigotmc.org>
2018-10-12 21:56:15 +0300

    Commit 6

--- NYGift/Form1.Designer.cs
+++ NYGift/Form1.Designer.cs
@@ -28,48 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttondel = new System.Windows.Forms.Button();
             this.buttonsort = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.buttonfind = new System.Windows.Forms.Button();
             this.buttoninfo = new System.Windows.Forms.Button();
             this.buttonadd = new System.Windows.Forms.Button();
+            this.radioCandy = new System.Windows.Forms.RadioButton();
+            this.radioChocolate = new System.Windows.Forms.RadioButton();
+            this.labelMenu = new System.Windows.Forms.Label();
+            this.labelMinSugar = new System.Windows.Forms.Label();
+            this.labelMaxSugar = new System.Windows.Forms.Label();
+            this.labelAdd = new System.Windows.Forms.Label();
+            this.labelTypeOfChocolate = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelSugar = new System.Windows.Forms.Label();
+            this.labelCalories = new System.Windows.Forms.Label();
+            this.labelWeight = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // buttondel
             // 
-            this.button1.Location = new System.Drawing.Point(369, 261);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttondel.Location = new System.Drawing.Point(433, 241);
+            this.buttondel.Name = "buttondel";
+            this.buttondel.Size = new System.Drawing.Size(99, 29);
+            this.buttondel.TabIndex = 0;
+            this.buttondel.Text = "Delete";
+            this.buttondel.UseVisualStyleBackColor = true;
+            this.buttondel.Click += new System.EventHandler(this.button1_Click);
             // 
             // buttonsort
             // 
             this.buttonsort.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.buttonsort.Location = new System.Drawing.Point(672, 12);
+            this.buttonsort.Location = new System.Drawing.Point(27, 76);
             this.buttonsort.Name = "buttonsort";
-            this.buttonsort.Size = new System.Drawing.Size(75, 23);
+            this.buttonsort.Size = new System.Drawing.Size(99, 29);
             this.buttonsort.TabIndex = 1;
             this.buttonsort.Text = "Sort";
             this.buttonsort.UseVisualStyleBackColor = true;
             this.buttonsort.Click += new System.EventHandler(this.buttonsort_Click);
             // 
-            // button3
+            // buttonfind
             // 
-            this.button3.Location = new System.Drawing.Point(343, 192);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonfind.Location = new System.Drawing.Point(27, 185);
+            this.buttonfind.Name = "buttonfind";
+            this.buttonfind.Size = new System.Drawing.Size(99, 29);
+            this.buttonfind.TabIndex = 2;
+            this.buttonfind.Text = "Find";
+            this.buttonfind.UseVisualStyleBackColor = true;
+            this.buttonfind.Click += new System.EventHandler(this.button3_Click);
             // 
             // buttoninfo
             // 
-            this.buttoninfo.Location = new System.Drawing.Point(99, 12);
+            this.buttoninfo.Location = new System.Drawing.Point(27, 41);
             this.buttoninfo.Name = "buttoninfo";
-            this.buttoninfo.Size = new System.Drawing.Size(75, 23);
+            this.buttoninfo.Size = new System.Drawing.Size(99, 29);
             this.buttoninfo.TabIndex = 3;
             this.buttoninfo.Text = "Info";
             this.buttoninfo.UseVisualStyleBackColor = true;
@@ -77,38 +97,255 @@
             // 
             // buttonadd
             // 
-            this.buttonadd.Location = new System.Drawing.Point(658, 192);
+            this.buttonadd.Location = new System.Drawing.Point(216, 405);
             this.buttonadd.Name = "buttonadd";
-            this.buttonadd.Size = new System.Drawing.Size(75, 23);
+            this.buttonadd.Size = new System.Drawing.Size(99, 29);
             this.buttonadd.TabIndex = 4;
             this.buttonadd.Text = "Add";
             this.buttonadd.UseVisualStyleBackColor = true;
+            this.buttonadd.Click += new System.EventHandler(this.buttonadd_Click);
+            // 
+            // radioCandy
+            // 
+            this.radioCandy.AutoSize = true;
+            this.radioCandy.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.radioCandy.Location = new System.Drawing.Point(141, 273);
+            this.radioCandy.Name = "radioCandy";
+            this.radioCandy.Size = new System.Drawing.Size(67, 20);
+            this.radioCandy.TabIndex = 5;
+            this.radioCandy.TabStop = true;
+            this.radioCandy.Text = "Candy";
+            this.radioCandy.UseVisualStyleBackColor = false;
+            this.radioCandy.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioChocolate
+            // 
+            this.radioChocolate.AutoSize = true;
+            this.radioChocolate.Location = new System.Drawing.Point(12, 273);
+            this.radioChocolate.Name = "radioChocolate";
+            this.radioChocolate.Size = new System.Drawing.Size(99, 20);
+            this.radioChocolate.TabIndex = 6;
+            this.radioChocolate.TabStop = true;
+            this.radioChocolate.Text = "Chocolate";
+            this.radioChocolate.UseVisualStyleBackColor = true;
+            this.radioChocolate.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // labelMenu
+            // 
+            this.labelMenu.AutoSize = true;
+            this.labelMenu.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.labelMenu.Location = new System.Drawing.Point(24, 9);
+            this.labelMenu.Name = "labelMenu";
+            this.labelMenu.Size = new System.Drawing.Size(44, 16);
+            this.labelMenu.TabIndex = 7;
+            this.labelMenu.Text = "Menu";
+            // 
+            // labelMinSugar
+            // 
+            this.labelMinSugar.AutoSize = true;
+            this.labelMinSugar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.labelMinSugar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelMinSugar.Font = new System.Drawing.Font("Stencil", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMinSugar.Location = new System.Drawing.Point(24, 125);
+            this.labelMinSugar.Name = "labelMinSugar";
+            this.labelMinSugar.Size = new System.Drawing.Size(86, 18);
+            this.labelMinSugar.TabIndex = 8;
+            this.labelMinSugar.Text = "Min Sugar";
+            this.labelMinSugar.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelMaxSugar
+            // 
+            this.labelMaxSugar.AutoSize = true;
+            this.labelMaxSugar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.labelMaxSugar.Font = new System.Drawing.Font("Stencil", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMaxSugar.Location = new System.Drawing.Point(24, 156);
+            this.labelMaxSugar.Name = "labelMaxSugar";
+            this.labelMaxSugar.Size = new System.Drawing.Size(88, 18);
+            this.labelMaxSugar.TabIndex = 9;
+            this.labelMaxSugar.Text = "Max sugar";
+            this.labelMaxSugar.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelAdd
+            // 
+            this.labelAdd.AutoSize = true;
+            this.labelAdd.Location = new System.Drawing.Point(9, 241);
+            this.labelAdd.Name = "labelAdd";
+            this.labelAdd.Size = new System.Drawing.Size(108, 16);
+            this.labelAdd.TabIndex = 10;
+            this.labelAdd.Text = "Add Sweetness";
+            this.labelAdd.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // labelTypeOfChocolate
+            // 
+            this.labelTypeOfChocolate.AutoSize = true;
+            this.labelTypeOfChocolate.Location = new System.Drawing.Point(9, 322);
+            this.labelTypeOfChocolate.Name = "labelTypeOfChocolate";
+            this.labelTypeOfChocolate.Size = new System.Drawing.Size(134, 16);
+            this.labelTypeOfChocolate.TabIndex = 11;
+            this.labelTypeOfChocolate.Text = "Type Of Chocolate";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(9, 353);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(43, 16);
+            this.labelName.TabIndex = 12;
+            this.labelName.Text = "Name";
+            // 
+            // labelSugar
+            // 
+            this.labelSugar.AutoSize = true;
+            this.labelSugar.Location = new System.Drawing.Point(9, 405);
+            this.labelSugar.Name = "labelSugar";
+            this.labelSugar.Size = new System.Drawing.Size(49, 16);
+            this.labelSugar.TabIndex = 14;
+            this.labelSugar.Text = "Sugar";
+            // 
+            // labelCalories
+            // 
+            this.labelCalories.AutoSize = true;
+            this.labelCalories.Location = new System.Drawing.Point(9, 431);
+            this.labelCalories.Name = "labelCalories";
+            this.labelCalories.Size = new System.Drawing.Size(68, 16);
+            this.labelCalories.TabIndex = 15;
+            this.labelCalories.Text = "Calories";
+            this.labelCalories.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // labelWeight
+            // 
+            this.labelWeight.AutoSize = true;
+            this.labelWeight.Location = new System.Drawing.Point(9, 380);
+            this.labelWeight.Name = "labelWeight";
+            this.labelWeight.Size = new System.Drawing.Size(58, 16);
+            this.labelWeight.TabIndex = 16;
+            this.labelWeight.Text = "Weight";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(249, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(474, 212);
+            this.listBox1.TabIndex = 17;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(161, 319);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 18;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 402);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 19;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(74, 377);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 20;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(74, 428);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 21;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(74, 350);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 22;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(131, 124);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.TabIndex = 23;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(131, 155);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 23);
+            this.textBox6.TabIndex = 24;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(789, 456);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.labelWeight);
+            this.Controls.Add(this.labelCalories);
+            this.Controls.Add(this.labelSugar);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelTypeOfChocolate);
+            this.Controls.Add(this.labelAdd);
+            this.Controls.Add(this.labelMaxSugar);
+            this.Controls.Add(this.labelMinSugar);
+            this.Controls.Add(this.labelMenu);
+            this.Controls.Add(this.radioChocolate);
+            this.Controls.Add(this.radioCandy);
             this.Controls.Add(this.buttonadd);
             this.Controls.Add(this.buttoninfo);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.buttonfind);
             this.Controls.Add(this.buttonsort);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttondel);
             this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Stencil", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "NYGift";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttondel;
         private System.Windows.Forms.Button buttonsort;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonfind;
         private System.Windows.Forms.Button buttoninfo;
         private System.Windows.Forms.Button buttonadd;
+        private System.Windows.Forms.RadioButton radioCandy;
+        private System.Windows.Forms.RadioButton radioChocolate;
+        private System.Windows.Forms.Label labelMenu;
+        private System.Windows.Forms.Label labelMinSugar;
+        private System.Windows.Forms.Label labelMaxSugar;
+        private System.Windows.Forms.Label labelAdd;
+        private System.Windows.Forms.Label labelTypeOfChocolate;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelSugar;
+        private System.Windows.Forms.Label labelCalories;
+        private System.Windows.Forms.Label labelWeight;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
 
--- NYGift/Form1.cs
+++ NYGift/Form1.cs
@@ -36,5 +36,45 @@ namespace NYGift
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonadd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
